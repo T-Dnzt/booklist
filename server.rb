3da6c2053f8f64daf56e591d0a101c7c4f2bec5b
T-Dnzt <thi.denizet@gmail.com>
2016-04-15 16:51:19 +0700

    Add create endpoint

--- server.rb
+++ server.rb
@@ -26,6 +26,26 @@ class Book
   scope :author, -> (author) { where(author: author) }
 end
 
+# Serializers
+class BookSerializer
+
+  def initialize(book)
+    @book = book
+  end
+
+  def as_json(*)
+    data = {
+      id: @book.id.to_s,
+      title: @book.title,
+      author: @book.author,
+      isbn: @book.isbn
+    }
+    data[:errors] = @book.errors if @book.errors
+    data
+  end
+
+end
+
 # Endpoints
 get '/' do
   'Welcome to BookList!'
@@ -37,6 +57,20 @@ namespace '/api/v1' do
     content_type 'application/json'
   end
 
+  helpers do
+    def base_url
+      @base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
+    end
+
+    def json_params
+      begin
+        JSON.parse(request.body.read)
+      rescue
+        halt 400, { message: 'Invalid JSON' }.to_json
+      end
+    end
+  end
+
   get '/books' do
     books = Book.all
 
@@ -44,7 +78,24 @@ namespace '/api/v1' do
       books = books.send(filter, params[filter]) if params[filter]
     end
 
-    books.to_json
+    books.map { |book| BookSerializer.new(book) }.to_json
+  end
+
+  get '/books/:id' do |id|
+    book = Book.where(id: id).first
+    halt(404, { message: 'Book Not Found'}.to_json) unless book
+    BookSerializer.new(book).to_json
+  end
+
+  post '/books' do
+    book = Book.new(json_params)
+    if book.save
+      response.headers['Location'] = "#{base_url}/api/v1/books/#{book.id}"
+      status 201
+    else
+      status 422
+      body BookSerializer.new(book).to_json
+    end
   end
 
 end
