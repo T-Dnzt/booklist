4887d3518461acc734746b73d3144714fc963ca6
T-Dnzt <thi.denizet@gmail.com>
2016-04-15 15:22:50 +0700

    Add Mongoid

--- server.rb
+++ server.rb
@@ -1,6 +1,27 @@
 # server.rb
 require 'sinatra'
+require 'mongoid'
 
+# DB Setup
+Mongoid.load! "mongoid.config"
+
+# Models
+class Book
+  include Mongoid::Document
+
+  field :title, type: String
+  field :author, type: String
+  field :isbn, type: String
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :isbn, presence: true
+
+  index({ title: 'text' })
+  index({ isbn: 1 }, { unique: true, name: "isbn_index" })
+end
+
+# Endpoints
 get '/' do
   'Welcome to BookList!'
 end
